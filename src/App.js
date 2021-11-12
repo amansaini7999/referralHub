5154245f3845d0567ed0804c49556cf3799bb8c8
astha1411 <thakurastha2@gmail.com>
2021-11-12 13:42:56 +0530

    job card, card template and button component for card added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import Header from "./Components/Header/Header";
-//import IntroCards from "./Components/IntroCards";
-import Footer from "./Components/Footer/Footer";
-import ProfilePage from "./Components/ProfilePage";
+import Cards from "./Components/cards";
 
 //install npm install react-icons --save
 const App = () => {
   return (
-    <>
-      <Header />
-      <main className="py-3 bg-light contextual">
-        <Container>
-          {/*<IntroCards /> */}
-          <ProfilePage isReferee={true} />
-        </Container>
-      </main>
-      <Footer />
-    </>
+    <Cards
+      heading="Create JOB"
+      desc="Kindly fill in the details to proceed"
+      cont="CreateJob"
+    />
   );
 };
 
